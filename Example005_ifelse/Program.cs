76915aab34be735803ee9f574fa9d1a7f1ee3fa2
Лёха <Alexbac@yandex.ru>
2023-02-26 21:06:41 +0300

    Ветвление ifelse

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите, пожалуйста имя? :  ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("О! Привет, Мария!!");
+}
+else
+{
+        Console.Write("Ну, здорово, ");
+        Console.WriteLine(username);
+}
