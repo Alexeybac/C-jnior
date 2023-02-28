443a69ed8553a66589d83534019b41a4cb90eecb
Лёха <Alexbac@yandex.ru>
2023-02-28 19:04:09 +0300

    Залил

--- Example005_ifelse/Program.cs
+++ Example005_ifelse/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.Write("Введите, пожалуйста имя? :  ");
 string username = Console.ReadLine();
-if(username.ToLower() == "маша")
+if(username.ToLower() == "таня")
 {
-    Console.WriteLine("О! Привет, Мария!!");
+Console.WriteLine("О! Привет, Танюшечка!!");
 }
 else
 {
-        Console.Write("Ну, здорово, ");
-        Console.WriteLine(username);
+Console.Write("Ну, здорово, ");
+Console.WriteLine(username);
 }
---
+++ Example009_massiv/Program.cs
@@ -0,0 +1,15 @@
+﻿int max(int arg1, int arg2, int arg3)
+{
+    int rezult = arg1;
+    if(arg2 > rezult) rezult = arg2;
+    if(arg3 > rezult) rezult = arg3;
+    return rezult;
+}
+
+int[] aray = {11,32,563,4,50,6,78,8,9000};
+
+int Max = max (max (aray[0], aray[1], aray[2]), 
+            max (aray[3], aray[4], aray[5]), 
+            max (aray[6], aray[7], aray[8]));
+
+Console.WriteLine(Max);
